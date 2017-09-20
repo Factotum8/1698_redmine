98429004094da336ec7f091093202c4fddf39f01
Your Name <you@example.com>
2017-09-20 20:27:01 +0300

    Init commit

---
+++ script.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+import os
+import xml.etree.ElementTree as et
+import argparse
+import codecs
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("-p", action='store', type=str, dest='pathToXml',
+                        help="Specify the path to the xml file.")
+arg_parser.add_argument("-f", action='store', type=str, dest='pathToSave',
+                        help="Specify the path to save wav files.")
+arg_parser.add_argument("-i", action='store', type=str, dest='pathToInfo',
+                        help="Specify the path to save information about files.")
+def main():
+
+    args = arg_parser.parse_args()
+
+    if not args.pathToXml:
+        print "Set the flag -p /path/to/xml"
+        return 1
+
+    if not os.path.exists(args.pathToXml):
+        print "Path to xml not exists :", args.pathToXml
+        return 1
+
+    if not os.path.exists(args.pathToSave):
+        print "Path to save not exists :", args.pathToSave
+        return 1
+
+    if not os.path.exists(args.pathToInfo):
+        print "Path to save not exists :", args.pathToInfo
+        return 1
+
+    file_info = codecs.open(args.pathToInfo,'w', encoding='utf8')
+
+    file_xml = os.path.join (args.pathToXml)
+
+    tree = et.ElementTree(file=file_xml)
+    rootNode = tree.getroot()
+
+    dict = {} # id:rawPath
+    for child in rootNode.iter('AudioRoot'):
+        dict[child.attrib['id']] = child.attrib['rawPath']
+        # print child.tag, child.attrib['id'], child.attrib['rawPath']
+
+
+    files_name = []
+    for child in rootNode.iter('Utt'):
+        # print child.attrib['audioRoot'],  child.attrib['audio']
+        # print "path=%s%s" %  (dict[child.attrib['audioRoot']], child.attrib['audio'])
+        # print "cp %s%s %s" %  (dict[child.attrib['audioRoot']], child.attrib['audio'], args.pathToSave)
+        os.system("cp %s%s %s" %  (dict[child.attrib['audioRoot']], child.attrib['audio'], args.pathToSave))
+        confidence = int(child.attrib['conf']) / 1000
+        file_info.write('%s;%s;%s;%d\n' % (child.attrib['audio'], child.attrib['rawText'], child.attrib['recValue'], confidence))
+        # os.system("cp %s%s %s" %  (dict[child.attrib['audioRoot']], child.attrib['audio'], args.pathToSave))
+        # file_info.write()
+
+if __name__ == '__main__':
+    main()
